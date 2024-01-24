4bf1932eb43b835736fe21bdfd8756e53dd52aa2
Oleg Natalushko <olegnatalushko@gmail.com>
2024-01-24 13:05:04 +0200

    test: LDP-2189: Implement watchdog auto fixture.

--- tests/helpers/ldp-test.js
+++ tests/helpers/ldp-test.js
@@ -1,7 +1,13 @@
 const base = require('@playwright/test');
+const expect= require('@playwright/test');
+const drupal = require('../helpers/drupal-commands');
 
 exports.test = base.test.extend({
   backendURL: ['http://admin--example.ldp-project.localdev.space', { option: true }],
   backendApiURL: ['http://admin--example.ldp-project.localdev.space/api', { option: true }],
-  frontendURL: ['http://example.ldp-project.localdev.space', { option: true }]
+  frontendURL: ['http://example.ldp-project.localdev.space', { option: true }],
+  watchdog: [async ({}, use, testInfo) => {
+    await use();
+    await expect(await drupal.checkWatchdogErrors(testInfo['_startWallTime'])).toEqual(0);
+  }, { scope: 'test', auto: true }]
 });
